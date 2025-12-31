205a8a29b6ec6677c81d2087484acb39256bb3b1
Tho Bui <51531118+thobui997@users.noreply.github.com>
2025-12-31 09:08:07 +0700

    Delete shared/pipes directory

--- shared/pipes/recordOrder.pipe.ts
+++
@@ -1,8 +0,0 @@
-import { Pipe, PipeTransform } from "@angular/core";
-
-@Pipe({ name: 'recordOrder', standalone: true })
-export class RecordOrderPipe implements PipeTransform {
-  transform(i: number, page: number, pageSize: number): number {
-    return (page - 1) * pageSize + (i + 1);
-  }
-}
